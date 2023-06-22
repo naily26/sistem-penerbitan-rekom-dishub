3523a64ebee4993f10acd22df03374b4454e711e
naily26 <nailymasruri@gmail.com>
2023-06-22 19:40:39 +0700

    edit layout

--- resources/views/layouts/guests/partials/footer.blade.php
+++ resources/views/layouts/guests/partials/footer.blade.php
@@ -12,27 +12,6 @@
                         &copy; Dinas Perhubungan Jawa Timur 2023 by Naily.
                     </p>
                 </div>
-                <div class="col-md-4">
-                    <nav id="sub-menu">
-                        <ul>
-                            <li>
-                                <a href="#">
-                                    FAQ's
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#">
-                                    Sitemap
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#">
-                                    Contact
-                                </a>
-                            </li>
-                        </ul>
-                    </nav>
-                </div>
             </div>
         </div>
     </div>
